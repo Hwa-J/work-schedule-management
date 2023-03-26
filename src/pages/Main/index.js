8a21889fbedd1c5ef963276ee056fbd08005992b
Hwa-J <hj1103.code@gmail.com>
2023-03-26 16:22:01 +0900

    feat: Main 페이지에 MainCalendar 컴포넌트 렌더링

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,12 @@
+import { MainCalendar } from 'components/Main/MainCalendar';
+
 const Main = () => {
-  return <div>메인페이지</div>;
+  return (
+    <div>
+      메인페이지
+      <MainCalendar />
+    </div>
+  );
 };
 
 export default Main;
