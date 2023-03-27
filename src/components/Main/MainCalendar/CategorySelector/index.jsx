8f713311bddb59f212d1bdcec637ab0dc2142c7a
Hwa-J <hj1103.code@gmail.com>
2023-03-28 04:22:16 +0900

    feat: 내 일정만 보기 checkbox 생성 및 스타일링

--- src/components/Main/MainCalendar/CategorySelector/index.jsx
+++ src/components/Main/MainCalendar/CategorySelector/index.jsx
@@ -1,5 +1,6 @@
 import { useEventsActions } from 'store/useEventsStore';
 import * as S from './style';
+import { Form } from 'react-bootstrap';
 
 export const CategorySelector = () => {
   const { filter } = useEventsActions();
@@ -8,13 +9,24 @@ export const CategorySelector = () => {
     filter(target.value);
   };
   return (
-    <S.CategorySelector
-      defaultValue={'모두 보기'}
-      onChange={handleSelectedValue}
-    >
-      <option>모두 보기</option>
-      <option>연차</option>
-      <option>당직</option>
-    </S.CategorySelector>
+    <S.FilterTool>
+      <Form.Select
+        defaultValue={'모두 보기'}
+        onChange={handleSelectedValue}
+        size="sm"
+      >
+        <option>모두 보기</option>
+        <option>연차</option>
+        <option>당직</option>
+      </Form.Select>
+      <S.SwitchForm>
+        <Form.Check
+          type="checkbox"
+          id="default-checkbox"
+          label="내 일정만 보기"
+          className="centerAlign"
+        />
+      </S.SwitchForm>
+    </S.FilterTool>
   );
 };
--- src/components/Main/MainCalendar/CategorySelector/style.js
+++ src/components/Main/MainCalendar/CategorySelector/style.js
@@ -1,11 +1,25 @@
 import styled from 'styled-components';
 import { Form } from 'react-bootstrap';
 
-export const CategorySelector = styled(Form.Select).attrs({
-  size: 'sm',
-  defaultValue: '모두보기',
+export const FilterTool = styled.div.attrs({
+  className: 'centerAlign',
 })`
-  width: auto;
+  padding-left: 10px;
   position: absolute;
   left: 0;
+`;
+export const SwitchForm = styled(Form)`
+  flex-shrink: 0;
+
+  label {
+    padding-left: 8px;
+  }
+  .form-check {
+    padding-left: 30px;
+    margin-bottom: 0;
+  }
+  .form-check .form-check-input {
+    margin-top: 0;
+    margin-left: 0;
+  }
 `;
