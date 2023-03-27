82196cb01540e71dacb491ec05001eeb4500457b
Hwa-J <hj1103.code@gmail.com>
2023-03-28 02:56:28 +0900

    feat: event(일정) 전역 스토어 생성 및 category filter 액션 구현

---
+++ src/store/useEventsStore.js
@@ -0,0 +1,46 @@
+import { create } from 'zustand';
+import { INIT, USER_ID } from 'api/mockup';
+
+// fetch 데이터 프론트에서 사용할 수 있도록 커스텀
+// 데이터 재설정 (user_account_id가 일치하는 것만 드래그 가능)
+// fetch 코드 작성시 내부에서 fetch 데이터 커스텀 후 반환 예정
+const customEventsData = INIT.map((event) => ({
+  ...event,
+  start: new Date(event.start),
+  end: new Date(event.end),
+
+  isDraggable: event.user_account_id === USER_ID,
+  allDay: true,
+}));
+
+const useEventsStore = create((set) => ({
+  events: customEventsData,
+  filteredEvents: [],
+  actions: {
+    // category 셀렉터에 따라 events 데이터 재계산
+    filter: (value) =>
+      set((state) => {
+        switch (value) {
+          case '모두 보기':
+            return { filteredEvents: state.events };
+          case '연차':
+            const dayOffData = state.events.filter(
+              (event) => event.category === '연차',
+            );
+            return { filteredEvents: dayOffData };
+          case '당직':
+            const watchDutyData = state.events.filter(
+              (event) => event.category === '당직',
+            );
+            return { filteredEvents: watchDutyData };
+          default:
+            return { filteredEvents: state.events };
+        }
+      }),
+  },
+}));
+
+export const useEvents = () => useEventsStore(({ events }) => events);
+export const useFilteredEvents = () =>
+  useEventsStore(({ filteredEvents }) => filteredEvents);
+export const useEventsActions = () => useEventsStore((state) => state.actions);
