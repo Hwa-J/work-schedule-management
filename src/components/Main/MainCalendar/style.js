bd325d6e6113d40b63221b9a478fa92d75702bac
Hwa-J <hj1103.code@gmail.com>
2023-03-28 05:06:40 +0900

    style: MainCalendar 일요일, 토요일 헤더 색상/ 일요일 날짜칸/이벤트(일정) 색상 스타일 변경

--- src/components/Main/MainCalendar/style.js
+++ src/components/Main/MainCalendar/style.js
@@ -6,11 +6,45 @@ export const DnDCalendar = withDragAndDrop(Calendar);
 
 // 이벤트 바 데이터 카테고리 값에 따른 배경 색상 지정
 export const MainCalendar = styled(DnDCalendar)`
-  .rbc-event.red {
-    background-color: #fe3e3e;
+  .rbc-month-view {
+    border: none;
   }
-  .rbc-event.green {
-    background-color: #26b026;
+  .rbc-month-view {
+    padding-top: 10px;
+  }
+  // 일요일, 토요일 헤더 색상
+  .rbc-header {
+    padding-bottom: 4px;
+    &:first-child span {
+      color: #ff4848;
+    }
+    &:last-child span {
+      color: #5a5af7;
+    }
+  }
+
+  // 일요일 날짜 바탕색
+  .rbc-row-bg div.rbc-day-bg:first-child,
+  .rbc-row div.rbc-date-cell:first-child {
+    background-color: #e6e6e6;
+    color: #999999;
+  }
+  // 전달, 다음달 날짜 바탕색
+  .rbc-off-range-bg {
+    background-color: #f4f4f4;
+  }
+  // 날짜칸
+  .rbc-date-cell {
+    padding-top: 5px;
+  }
+
+  // 당직 이벤트 색상
+  .rbc-event.red {
+    background-color: #f87688;
+  }
+  // 연차 이젠트 색상
+  .rbc-event.blue {
+    background-color: #6db2f4;
   }
 `;
 
@@ -18,5 +52,5 @@ export const MainCalendar = styled(DnDCalendar)`
 export const EventBar = styled.div`
   display: flex;
   align-items: center;
-  padding: 4px;
+  padding: 2px;
 `;
