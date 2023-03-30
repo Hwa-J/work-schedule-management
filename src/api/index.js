03f6a8b5f1e3197a17e00b9f673eb987e81b7dd9
05Castle <kos4005@gmail.com>
2023-03-30 18:24:50 +0900

    Feat: add default HTTP header

--- src/api/index.js
+++ src/api/index.js
@@ -3,5 +3,7 @@ import axios from 'axios';
 export const instance = axios.create({
   baseURL: 'http://localhost:4000',
 });
-
-// instance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+const token = localStorage.getItem('access_token');
+instance.defaults.headers.common['Authorization'] = token
+  ? `Bearer ${token}`
+  : null;
