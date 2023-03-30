f4ca70235d80faa8163cc449f1fb9ff93c2a0f4d
Hwa-J <hj1103.code@gmail.com>
2023-03-30 19:14:37 +0900

    feat: 삭제 확인 모달창 제어용 deleteEventModal 상태 값 및 showDeleteEventModal 액션 추가

--- src/store/useModalStore.js
+++ src/store/useModalStore.js
@@ -1,17 +1,24 @@
-const { create } = require('zustand');
+import { create } from 'zustand';
 
 const useModalsStore = create((set) => ({
   addEventNomalModal: false,
+  deleteEventModal: false,
 
   actions: {
     showAddEventNomalModal: (value) =>
       set(() => ({
         addEventNomalModal: value,
       })),
+    showDeleteEventModal: (value) =>
+      set(() => ({
+        deleteEventModal: value,
+      })),
   },
 }));
 
 export const useAddModals = () =>
   useModalsStore(({ addEventNomalModal }) => addEventNomalModal);
+export const useDeleteModal = () =>
+  useModalsStore(({ deleteEventModal }) => deleteEventModal);
 
 export const useModalsActions = () => useModalsStore((state) => state.actions);
