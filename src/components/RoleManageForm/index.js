04e74fc84477c3ac5f9cd8813f1042a44a897309
05Castle <kos4005@gmail.com>
2023-03-31 21:45:16 +0900

    Feat: disabled update button until fill the form

--- src/components/RoleManageForm/index.js
+++ src/components/RoleManageForm/index.js
@@ -4,13 +4,19 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { FormLabel } from 'react-bootstrap';
 import SearchBar from 'components/SearchBar';
-import { useSearchStore } from 'store/store';
+import useSearchStore from 'store/useSearchStore';
+import { useState } from 'react';
 
 const RoleManageForm = () => {
   const { name, email } = useSearchStore();
+  const [selected, setSelected] = useState(false);
+  const setSearchName = useSearchStore((state) => state.setName);
+  const setSearchEmail = useSearchStore((state) => state.setEmail);
 
   const handleUpdate = (e) => {
     e.preventDefault();
+    setSearchName('');
+    setSearchEmail('');
     alert('권한이 업데이트 되었습니다.');
   };
 
@@ -52,6 +58,9 @@ const RoleManageForm = () => {
               label="일반 유저"
               name="formHorizontalRadios"
               id="formHorizontalRadios1"
+              onClick={() => {
+                setSelected(true);
+              }}
             />
           </Col>
           <Col sm={4}>
@@ -62,13 +71,22 @@ const RoleManageForm = () => {
               label="관리자"
               name="formHorizontalRadios"
               id="formHorizontalRadios2"
+              onClick={() => {
+                setSelected(true);
+              }}
             />
           </Col>
         </Form.Group>
         <div className="centerAlign">
-          <Button type="submit" onClick={handleUpdate}>
-            업데이트
-          </Button>
+          {name && selected === true ? (
+            <Button type="submit" onClick={handleUpdate}>
+              업데이트
+            </Button>
+          ) : (
+            <Button type="submit" disabled>
+              업데이트
+            </Button>
+          )}
         </div>
       </Form>
     </>
