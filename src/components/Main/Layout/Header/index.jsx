249f04210c921ed241a8e8e3b51cb3cdc0f70db5
05Castle <kos4005@gmail.com>
2023-03-31 21:08:25 +0900

    Feat: reflect user info on header

--- src/components/Main/Layout/Header/index.jsx
+++ src/components/Main/Layout/Header/index.jsx
@@ -1,12 +1,18 @@
 import * as S from './style';
 import LogoutBtn from 'components/Common/LogoutBtn';
+import useLoggedUserStore from 'store/useLoggedUserStore';
 
 export const Header = () => {
+  const { role, name } = useLoggedUserStore();
   return (
     <S.Header>
-      <S.RoleInfo>관리자</S.RoleInfo>
+      {role === 'ADMIN' ? (
+        <S.RoleInfo color="#ffafa1">관리자</S.RoleInfo>
+      ) : (
+        <S.RoleInfo color="#e7e7e7">일반</S.RoleInfo>
+      )}
       <span>
-        안녕하세요! <S.Name>User</S.Name>님
+        안녕하세요! <S.Name>{name}</S.Name>님
       </span>
       {/* <S.RoleInfo>{userStore.role}</S.RoleInfo> */}
       {/* <span>안녕하세요! <S.Name>{userStore.name}</S.Name>님</span> */}
