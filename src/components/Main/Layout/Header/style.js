aa0a0bd273ee2d31e905e7cad42e8c16b949fcbd
Hwa-J <hj1103.code@gmail.com>
2023-04-04 16:35:16 +0900

    style: 클래스 show를 이용한 사이드바 슬라이드 스타일링

--- src/components/Main/Layout/Header/style.js
+++ src/components/Main/Layout/Header/style.js
@@ -1,11 +1,38 @@
 import styled from 'styled-components';
+import { Row } from 'react-bootstrap';
 
 // Header 스타일
-export const Header = styled.header.attrs({
+export const HeaderRow = styled(Row).attrs({ className: 'centerAlign' })`
+  justify-content: space-between;
+  .d-block.d-lg-none.d-xl-none {
+    flex-grow: 0;
+  }
+  svg {
+    font-size: 18px;
+    cursor: pointer;
+  }
+`;
+export const HeaderSideBarContainer = styled.div`
+  width: 100%;
+  height: calc(100vh - 35px);
+  padding-top: 10px;
+  position: absolute;
+  overflow: hidden;
+  transition: 0.5s;
+  visibility: hidden;
+  transform: translateX(-100%);
+  z-index: 5;
+
+  &.show {
+    visibility: visible;
+    transform: translateX(-14px);
+  }
+`;
+
+export const UserInfoContainer = styled.div.attrs({
   className: 'centerAlign',
 })`
   height: 50px;
-  margin-right: 40px;
   justify-content: end;
 
   span {
@@ -18,8 +45,6 @@ export const RoleInfo = styled.div`
   border-radius: 4px;
   font-weight: 600;
   background-color: ${(props) => props.color};
-  /* background-color: ${({ role }) =>
-    role === '관리자' ? '#ffafa1' : '#e7e7e7'}; */
 `;
 export const Name = styled.span`
   font-weight: 600;
