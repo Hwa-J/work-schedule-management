2e2773f361bf7d4534b3e6a07e024d9bb01963ac
Hwa-J <hj1103.code@gmail.com>
2023-04-04 16:31:14 +0900

    feat: 모바일 사이즈 경우 햄버거 메뉴 활성화 및 메뉴 열고 닫기 기능 구현

--- src/components/Main/Layout/Header/index.jsx
+++ src/components/Main/Layout/Header/index.jsx
@@ -1,22 +1,38 @@
-import * as S from './style';
-import LogoutBtn from 'components/Common/LogoutBtn';
+import { useState } from 'react';
 import useLoggedUserStore from 'store/useLoggedUserStore';
+import { SideBar } from '../SideBar';
+import LogoutBtn from 'components/Common/LogoutBtn';
+import { Col } from 'react-bootstrap';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import * as S from './style';
 
 export const Header = () => {
   const { role, name } = useLoggedUserStore();
+  const [isShow, setIsShow] = useState(false);
+
   return (
-    <S.Header>
-      {role === 'ADMIN' ? (
-        <S.RoleInfo color="#ffafa1">관리자</S.RoleInfo>
-      ) : (
-        <S.RoleInfo color="#e7e7e7">일반</S.RoleInfo>
-      )}
-      <span>
-        안녕하세요! <S.Name>{name}</S.Name>님
-      </span>
-      {/* <S.RoleInfo>{userStore.role}</S.RoleInfo> */}
-      {/* <span>안녕하세요! <S.Name>{userStore.name}</S.Name>님</span> */}
-      <LogoutBtn />
-    </S.Header>
+    <header>
+      <S.HeaderRow>
+        <Col className="d-block d-lg-none d-xl-none">
+          <GiHamburgerMenu onClick={() => setIsShow(!isShow)} />
+          <S.HeaderSideBarContainer className={isShow ? 'show' : ''}>
+            <SideBar />
+          </S.HeaderSideBarContainer>
+        </Col>
+        <Col>
+          <S.UserInfoContainer>
+            {role === 'ADMIN' ? (
+              <S.RoleInfo color="#ffafa1">관리자</S.RoleInfo>
+            ) : (
+              <S.RoleInfo color="#e7e7e7">일반</S.RoleInfo>
+            )}
+            <span>
+              안녕하세요! <S.Name>{name}</S.Name>님
+            </span>
+            <LogoutBtn />
+          </S.UserInfoContainer>
+        </Col>
+      </S.HeaderRow>
+    </header>
   );
 };
