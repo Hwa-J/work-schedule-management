d67f224d4461f58eb38f07b7ef78baa135b9bc06
Hwa-J <hj1103.code@gmail.com>
2023-04-12 03:15:00 +0900

    feat: 일정 패치쿼리 데이터 필터링 select 옵션기능 구현 및 year, month 인수값 훅 내부로 이동

--- src/util/hooks/useGetMonthEvents.js
+++ src/util/hooks/useGetMonthEvents.js
@@ -1,12 +1,40 @@
 import { fetchMonthEvents } from 'api';
+import { useCallback } from 'react';
 import { useQuery } from 'react-query';
 import useLoggedUserStore from 'store/useLoggedUserStore';
+import { useMonth, useYear } from 'store/useYearMonthStore';
+import { filteredEvents, filteredMyEvents } from 'util/getFilteredEvents';
 
-export const useGetMonthEvents = (year, month) => {
+export const useGetMonthEvents = (filter, showMyEvents) => {
+  // 현재 캘린더의 날짜 정보 가져오기
+  const year = useYear();
+  const month = useMonth();
+
   const id = useLoggedUserStore(({ id }) => id);
   const role = useLoggedUserStore(({ role }) => role);
 
-  return useQuery(['events', { year, month }], () =>
-    fetchMonthEvents({ id, role }, year, month),
+  const selectedfilter = useCallback(
+    (events) => {
+      if (showMyEvents) {
+        const filtered =
+          filter !== '모두 보기' ? filteredEvents(events, filter) : events;
+        return filteredMyEvents(filtered, id);
+      } else {
+        return filteredEvents(events, filter);
+      }
+    },
+    [filter, showMyEvents, id],
   );
+
+  const { data: events } = useQuery(
+    ['events', { year, month }],
+    () => fetchMonthEvents({ id, role }, year, month),
+    {
+      select: (data) => {
+        const filteredData = selectedfilter(data);
+        return filteredData;
+      },
+    },
+  );
+  return { events };
 };
